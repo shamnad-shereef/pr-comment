c2b05e6d5b7c3a9a6507710c0208436c085016bf
Shamnad A S <shamnad.shereef@trenser.com>
2025-09-15 18:36:26 +0530

    update action

--- src/main.js
+++ src/main.js
@@ -1,27 +1,75 @@
 import * as core from '@actions/core'
-import { wait } from './wait.js'
+import * as core from '@actions/github'
 
-/**
- * The main function for the action.
- *
- * @returns {Promise<void>} Resolves when the action is complete.
- */
-export async function run() {
+async function run() {
   try {
-    const ms = core.getInput('milliseconds')
+    const owner = core.getInput('owner', { required: true })
+    const repo = core.getInput('repo', { required: true })
+    const pr_number = core.getInput('pr_number', { required: true })
+    const token = core.getInput('token', { required: true })
 
-    // Debug logs are only output if the `ACTIONS_STEP_DEBUG` secret is true
-    core.debug(`Waiting ${ms} milliseconds ...`)
+    const octokit = new github.getOctokit(token)
 
-    // Log the current timestamp, wait, then log the new timestamp
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const { data: changedFiles } = await octokit.rest.pulls.listFiles({
+      owner,
+      repo,
+      pull_number: pr_number
+    })
 
-    // Set outputs for other workflow steps to use
-    core.setOutput('time', new Date().toTimeString())
+    let diffData = {
+      addition: 0,
+      deletions: 0,
+      changes: 0
+    }
+
+    diffData = changedFiles.reduce((acc, file) => {
+      acc.additions += file.additions
+      acc.deletions += file.deletions
+      acc.changes += file.changes
+      return acc
+    }, diffData)
+
+    await octokit.rest.issues.createComment({
+      owner,
+      repo,
+      issue_number: pr_number,
+      body: `
+        Pull request #${pr_number} has be updated with: \n
+        - ${diffData.changes} changes \n
+        - ${diffData.additions} additions \n
+        - ${diffData.deletions} deletions
+      `
+    })
+
+    for (const file of changedFiles) {
+      const fileExtention = file.filename.split('.').pop()
+      let label = ''
+      switch (fileExtention) {
+        case 'md':
+          label = 'markdown'
+          break
+        case 'js':
+          label = 'javascript'
+          break
+        case 'yml':
+          label = 'yaml'
+          break
+        case 'yaml':
+          label = 'yaml'
+          break
+        default:
+          label = 'noextension'
+      }
+      await octokit.rest.issues.addLabels({
+        owner,
+        repo,
+        issue_number: pr_number,
+        labels: [label]
+      })
+    }
   } catch (error) {
-    // Fail the workflow run if an error occurs
-    if (error instanceof Error) core.setFailed(error.message)
+    core.setFailed(error.message)
   }
 }
+
+run()
